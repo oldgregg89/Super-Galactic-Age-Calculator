2a09507dc3c096b049006b6bae960ee1656e37bc
Ian Gregg <iangregg188@gmail.com>
2020-06-26 10:16:01 -0700

    worked on the testing of the galactic age

---
+++ __tests__/Galactic-Age.test.js
@@ -0,0 +1,35 @@
+import { galacticAge } from '../src/Galactic-Age.js'
+
+describe ('galacticAge', () => {
+  let venusAge;
+  let marsAge;
+  let mercuryAge;
+  let jupiterAge;
+  let lifeExpectancy;
+  let pastLifeExpectancy;
+
+  beforeEach(() => {
+    venusAge = new galacticAge
+    marsAge = new galacticAge
+    mercuryAge = new galacticAge
+    jupiterAge = new galacticAge
+    lifeExpectancy = new galacticAge
+    pastLifeExpectancy = new galacticAge
+  }
+
+  text( , =>{
+
+  }
+
+  // text( , =>{
+    
+  // }
+
+  // text( , =>{
+    
+  // }
+
+  // text( , =>{
+    
+  // }
+});
