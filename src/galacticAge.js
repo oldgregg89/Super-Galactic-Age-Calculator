c6fd2ea1cc1879a7c8c1624cb96de61368bf9568
Ian Gregg <iangregg188@gmail.com>
2020-07-07 18:33:26 -0700

    Refactored the expectency functions to pass expected red green behavior, they now pass.

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -9,7 +9,7 @@ export class GalacticAge {
   }
 
   venusExpectency(){
-    return (this.lifeEx/.62)-this.venusAge()
+    return (this.lifeEx/.62)
   }
 
   marsAge() {
@@ -17,7 +17,7 @@ export class GalacticAge {
   }
 
   marsExpectency(){
-    return this.lifeEx-this.marsAge()
+    return (this.lifeEx/1.88)
   }
 
 
@@ -26,7 +26,7 @@ export class GalacticAge {
   }
 
   mercuryExpectency(){
-    return this.lifeEx-this.mercuryAge()
+    return (this.lifeEx/.24)
   }
 
   jupiterAge() {
@@ -34,7 +34,7 @@ export class GalacticAge {
   }
 
   jupiterExpectency(){
-    return this.lifeEx-this.jupiterAge()
+    return (this.lifeEx/11.86)
   }
 
 }
